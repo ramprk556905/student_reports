dd89c9879a1dcd7b67dc282cec4910a871a4607c
DAI\ramkumar.pandidurai <ramkumar.pandidurai>
2024-10-01 09:45:19 +0530

    Changed the Database URL

--- server/databases.py
+++ server/databases.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ramprk97@localhost:5432/studentsreport"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ramprk97@localhost:5432/studentsreport"
+SQLALCHEMY_DATABASE_URL = "postgresql://studentsreport_user:ZRWFo9GJigh3qZNDsMVG8eQmZWxufeR6@dpg-crt7mi3tq21c73dljprg-a/studentsreport"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
